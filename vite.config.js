56d7f04694c1837440cdc3730a78b9590242ef3e
Sergio Caique <scaique080@gmail.com>
2023-09-03 20:58:41 -0300

    Atualização do vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/IntensivaoJavaScript",
+  base: "/IntensivaoJavaScript/",
   plugins: [],
   resolve: {
     /*something*/
